2116319b39db63c28e39227b9537070c71402c6a
Lindrew <66846143+GReturn@users.noreply.github.com>
2026-05-27 10:46:55 +0800

    config(backend): switch to SQLite and configure ALLOWED_HOSTS for mobile development

--- backend/Amping/settings.py
+++ backend/Amping/settings.py
@@ -37,6 +37,9 @@ if DEBUG:
 
     ALLOWED_HOSTS = [
         'localhost',
+        '127.0.0.1',
+        '10.0.2.2',
+        '*',
     ]
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:5173",
@@ -108,13 +111,17 @@ WSGI_APPLICATION = 'Amping.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME'),
+    #     'USER': os.getenv('DB_USER'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD'),
+    #     'HOST': os.getenv('DB_HOST'),
+    #     'PORT': os.getenv('DB_PORT'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
