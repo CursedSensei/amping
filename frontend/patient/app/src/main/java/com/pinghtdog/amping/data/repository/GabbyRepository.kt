64770f465d03a2508e5fbe1b6e6a24797b0b5975
Lindrew <66846143+GReturn@users.noreply.github.com>
2026-05-31 10:35:55 +0800

    refactor: remove unused imports in GabbyRepository

--- frontend/patient/app/src/main/java/com/pinghtdog/amping/data/repository/GabbyRepository.kt
+++ frontend/patient/app/src/main/java/com/pinghtdog/amping/data/repository/GabbyRepository.kt
@@ -18,13 +18,10 @@ import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
 import io.ktor.client.request.header
-import io.ktor.client.request.forms.submitFormWithBinaryData
-import io.ktor.client.request.forms.formData
 import io.ktor.client.statement.bodyAsText
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
 import io.ktor.http.isSuccess
 import io.ktor.serialization.kotlinx.json.json
